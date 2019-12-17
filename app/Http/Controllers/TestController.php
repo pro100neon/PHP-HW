3483366ea0e82ec6cb188bb213916c58b8010a70
Chupakabra47 <201dimon@mail.ru>
2019-12-17 14:33:02 +0700

    task 4 done

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //
+    private $name = 'Petya';
+    private $age = 18;
+    private $isMen = true;
+
+    private function getType()
+    {
+        return ['name' => gettype($this->name),
+                'age' => gettype($this->age),
+                'isMen' => gettype($this->isMen)];
+    }
+
+    public function getOne()
+    {
+        return $this->getType();
+    }
+
+    public function setName(String $var)
+    {
+        $this->name = $var;
+    }
+
+    public function setAge(Int $var)
+    {
+        $this->age = $var;
+    }
+
+    public function setIsMen(bool $var)
+    {
+        $this->isMen = $var;
+    }
 }
+
+
