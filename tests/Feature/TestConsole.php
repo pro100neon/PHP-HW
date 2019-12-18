ed863302c5bcadfddd73d6ceac1cff97d8aff64e
Chupakabra47 <201dimon@mail.ru>
2019-12-18 18:53:12 +0700

    hw8 4 task is done

---
+++ tests/Feature/TestConsole.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TestConsole extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->artisan('test:console')
+            ->expectsOutput('not my first test')
+            ->assertExitCode(0);
+    }
+}
