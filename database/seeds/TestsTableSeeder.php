e982906d4e95d4c997a3c236740c74c322955e48
Chupakabra47 <201dimon@mail.ru>
2019-12-18 13:29:39 +0700

    hw7 3 task is done and fix errors

---
+++ database/seeds/TestsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+class TestsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Models\Test::class, 10)->create()->each(function ($tests)
+        {
+            $tests->save(factory(App\Models\Test::class)->make());
+        });
+    }
+}
