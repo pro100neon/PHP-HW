85f0ebfc7ffd533fc600cd150a23776bfc10bb53
Chupakabra47 <201dimon@mail.ru>
2019-12-18 13:59:26 +0700

    hw7 5 task is done

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -8,4 +8,11 @@ class Test extends Model
 {
     protected $fillable = ['text', 'is_enabled'];
     protected $casts = ['is_enabled' => 'boolean'];
+
+    public static function task5InHW()
+    {
+        $maxId = Test::orderBy('id','desc')->first();
+        Test::where('id', $maxId)->update(['text' => 'So what about pepito?', 'is_enabled' => 1]);
+        return $maxId;
+    }
 }
